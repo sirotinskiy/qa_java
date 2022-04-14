c3fccccb8662d16553f78e596ab477e7fa633c73
Vladimir Sirotinskiy <rarka2009@gmail.com>
2022-04-14 22:09:12 +0300

    Добавлено дополнительное задание 2 спринта

---
+++ src/main/java/com/example/Alex.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.util.List;
+
+public class Alex extends Lion{
+
+    public Alex(Feline feline) throws Exception {
+        super(feline, "Самец");
+    }
+
+    @Override
+    public int getKittens() {
+        return 0;
+    }
+
+    public List<String> getFriends(){
+        return List.of("Марти", "Глории", "Мелман");
+    }
+
+    public String getPlaceOfLiving(){
+        return "Нью-Йоркский зоопарк";
+    }
+}
